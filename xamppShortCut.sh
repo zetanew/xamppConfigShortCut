2c92a01bedd91a3a62319fba0474155f4dedb43b
Volkan <volkankanat.mdbf17@iste.edu.tr>
2023-06-21 18:18:10 +0300

    first commit

---
+++ xamppShortCut.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo '#####XAMPP SETTINGS ALIAS######' >> ~/.bashrc
+echo 'alias phpset="sudo /opt/lampp/manager-linux-x64.run"' >> ~/.bashrc
+echo '##########################' >> ~/.bashrc 
+source ~/.bashrc
+echo ""
+echo "You can now type "phpset" to terminal to open xampp settings"
+echo "         _                      _           
+ _______| |_ __ _  ___ ___   __| | ___ _ __ 
+|_  / _ \ __/ _\` |/ __/ _ \ / _\` |/ _ \ '__|
+ / /  __/ || (_| | (_| (_) | (_| |  __/ |   
+/___\___|\__\__,_|\___\___/ \__,_|\___|_|   
+                                            
+"

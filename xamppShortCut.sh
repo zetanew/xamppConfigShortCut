1543b75e001a754010ddd201f12e65ba4becb452
Volkan <volkankanat.mdbf17@iste.edu.tr>
2023-07-12 09:01:50 +0300

    text changed

--- xamppShortCut.sh
+++ xamppShortCut.sh
@@ -4,7 +4,7 @@ echo 'alias phpset="sudo /opt/lampp/manager-linux-x64.run"' >> ~/.bashrc
 echo '##########################' >> ~/.bashrc 
 source ~/.bashrc
 echo ""
-echo "You can now type "phpset" to terminal to open xampp settings"
+echo "You can now type "phpset" to terminal to open xampp settings..."
 echo "         _                      _           
  _______| |_ __ _  ___ ___   __| | ___ _ __ 
 |_  / _ \ __/ _\` |/ __/ _ \ / _\` |/ _ \ '__|
